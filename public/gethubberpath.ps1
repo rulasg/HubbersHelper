301424506187da72192e2389af914f75493fa4c8
Raúl (Dibildos) González <rulasg@github.com>
2025-10-03 08:59:38 +0200

    feat(getHubberPath): implement Get-HubberPath function to retrieve hubber hierarchy

---
+++ public/gethubberpath.ps1
@@ -0,0 +1,23 @@
+function Get-HubberPath{
+    [CmdletBinding()]
+    param (
+        [Parameter(ValueFromPipeline,ValueFromPipelineByPropertyName,Position=0)][string]$Handle
+    )
+
+    $hubber = Get-Hubber -Handle $Handle
+
+    if($null -eq $hubber){
+        throw "Hubber with handle '$Handle' not found"
+    }
+
+    $ret= @()
+    $h = $hubber
+
+    while($h.level -ge 0){
+        $ret += $h
+        $h = $h.manager
+    }
+
+    return $ret
+
+} Export-ModuleMember -Function Get-HubberPath
